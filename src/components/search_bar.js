cb0aec290dd143440f38038a59f45050dc8bf47a
chmeshra <chmeshra@visa.com>
2016-03-08 16:47:33 -0800

    Adding Template literals for youtube video urls, ES6 destructuring and object pattern

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -4,7 +4,7 @@ class SearchBar extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {term: 'Search Youtube'};
+    this.state = {term: ''};
   }
   render() {
     return (
--- src/index.js
+++ src/index.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
-// Youtube API key
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyAtjCBe7M4rp5Qct2xMc6Hf-gw9J-NaF-g';
 
-// Create a new component. This component should produce
-// some HTML
-
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      videos:[],
+      selectedVideo: null
+     };
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+      //this.setState({ videos: videos });
+    });
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo}/>
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos= {this.state.videos}/>
+      </div>
+    );
+  }
 }
 // Take this component's generated HTML and put it
 // on the page (in the DOM)
